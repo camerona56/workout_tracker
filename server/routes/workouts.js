99100d4cd63742a30138048d50fb00612b75349a
camerona56 <cameronadamson56@gmail.com>
2023-02-27 13:20:25 +0000

    models & schemas

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Workout = require("../models/workoutModel");
 
 const router = express.Router();
 
@@ -13,18 +14,25 @@ router.get("/:id", (req, res) => {
 });
 
 // POST a new workout
-router.post('/', (req, res) => {
-  res.send({mssg: "POST a new workout"})
-})
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-  res.send({mssg: "DELETE a new workout"})
-})
+router.delete("/:id", (req, res) => {
+  res.send({ mssg: "DELETE a new workout" });
+});
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-  res.send({mssg: "UPDATE a new workout"})
-})
+router.patch("/:id", (req, res) => {
+  res.send({ mssg: "UPDATE a new workout" });
+});
 
 module.exports = router;
